6ecad3c41c8a70eb4d3b120ba15202f70199503e
Vang Mai Quyet <maiquyetvang2002@gmail.com>
2024-06-19 16:52:18 +0700

    AuthService

---
+++ MicroService/RegistryServer/src/test/java/com/udpt_banve/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udpt_banve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
